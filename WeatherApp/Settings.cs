843050578195366554f46c065e65c48d8e7aed1a
carvalho57 <gabrielfeocarvalho@gmail.com>
2020-09-29 16:12:43 -0300

    Retirando api key do settings

--- WeatherApp/Settings.cs
+++ WeatherApp/Settings.cs
@@ -2,7 +2,7 @@ namespace WeatherApp
 {
     public static class Settings
     {
-        public static string APIKEY = "6fda32bbc2ce0063dea0d1bba30c1b23";
+        public static string APIKEY = "YOUR API KEY HERE";
         public static string WeatherUri = $"api.openweathermap.org/data/2.5/weather?q=name&units=metric&lang=pt_br&appid={APIKEY}";
         public static string GeoWeatherUri = $"api.openweathermap.org/data/2.5/weather?lat=latvalue&lon=lonvalue&units=metric&lang=pt_br&appid={APIKEY}";
     }
